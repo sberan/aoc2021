507111f254249b1fe71d9de5b8bebc5e9504cd49
Sam Beran <sberan@users.noreply.github.com>
2021-12-04 22:00:38 +0000

    init

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,10 +5,16 @@
 
 package com.mycompany.app;
 
+import com.google.common.collect.Multiset;
+import com.google.common.collect.HashMultiset;
+
 public class App 
 {
     public static void main( String[] args )
     {
+        Multiset<String> m = HashMultiset.create();
+
+
         System.out.println( "Hello Remote World!" );
     }
 }
