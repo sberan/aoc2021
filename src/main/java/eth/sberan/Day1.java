7b06fadcf4a130f48d7590e68e4d495fca9a4160
Sam Beran <sberan@users.noreply.github.com>
2021-12-05 00:12:58 +0000

    day 1: part 2

--- src/main/java/eth/sberan/Day1.java
+++ src/main/java/eth/sberan/Day1.java
@@ -6,23 +6,23 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import com.google.common.collect.Streams;
-
 import org.jooq.lambda.Seq;
-import org.jooq.lambda.Window;
 
 public class Day1 {
+
+  static <T> Stream<List<T>> slidingWindow(Stream<T> input, int size) {
+    return Seq.seq(input).sliding(size).map(w -> w.toList()).stream();
+  }
+
   public static void main(String[] args) throws Exception {
     Path inputFile = Path.of(Day1.class.getResource("input.txt").toURI());
     Stream<Integer> measurements = Files.readAllLines(inputFile).stream()
       .map((line) -> Integer.parseInt(line));
 
-    
-    Seq<List<Integer>> pairs = Seq.seq(measurements)
-      .sliding(2)
-      .map(w -> w.collect(Collectors.toList()));
+    Stream<Integer> sums = slidingWindow(measurements, 3)
+      .map(w -> w.stream().collect(Collectors.summingInt(Integer::intValue)));
 
-    long countDeeper = pairs
+    long countDeeper = slidingWindow(sums, 2)
       .filter(pair -> pair.get(0) < pair.get(1))
       .count();
 
